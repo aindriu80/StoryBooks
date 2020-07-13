b6f2b52580941c0dda9ac373d7bdd9c71a13a455
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2020-07-13 21:07:25 +0100

    Setting up basic routes and middleware

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 // Load config
@@ -10,6 +12,19 @@ dotenv.config({
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+// Handlebars
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+// Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
 
--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useNewUrlParser: true,
-            useFindAndModify: false
+            useFindAndModify: false,
+            useUnifiedTopology: true
         })
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (err) {
