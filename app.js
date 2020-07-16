cbf84cfe8d5a2d053e2ab56d2d2988a9a7e7f361
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2020-07-16 17:39:09 +0100

    Implementing Google OAuth & PassportJS

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require("./config/db");
 
 // Load config
@@ -10,6 +12,8 @@ dotenv.config({
   path: "./config/config.env",
 });
 
+// Passport config
+require('./config/passport')(passport)
 connectDB();
 
 const app = express();
@@ -28,12 +32,23 @@ app.engine(
 );
 app.set("view engine", ".hbs");
 
-// Static Folder
+// Sessions
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}))
 
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static Folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 3000;
 
