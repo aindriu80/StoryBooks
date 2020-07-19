53cca68881ecdb14f941f074c0ad98daebe32423
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2020-07-19 21:39:09 +0100

    Adding protective routes

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const {
+    ensureAuth,
+    ensureGuest
+} = require('../middleware/auth')
 
 // @desc    Login/Landing page
 // @route   Get/
 
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login'
     })
 })
 // @desc    Dashboard
 // @route   GET /dashboard
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuth, (req, res) => {
+    console.log(req.user)
     res.render('dashboard')
 })
 
