552d8ab98462a8c998628c29056767a385de8fef
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2020-07-21 10:32:26 +0100

    Initial Add Story Page, routing

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const express = require("express");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
-const passport = require('passport');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session)
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const connectDB = require("./config/db");
 
 // Load config
@@ -15,7 +15,7 @@ dotenv.config({
 });
 
 // Passport config
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 connectDB();
 
 const app = express();
@@ -35,18 +35,20 @@ app.engine(
 app.set("view engine", ".hbs");
 
 // Sessions
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+    }),
   })
-}))
+);
 
 // Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static Folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -54,6 +56,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 3000;
 
