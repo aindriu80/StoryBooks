81b57c68a47fa2d46d7c23a801db18cab613e8dc
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2020-07-24 16:06:07 +0100

    Creating the edit page

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,32 @@
-const express = require('express')
-const router = express.Router()
-const {
-    ensureAuth,
-    ensureGuest
-} = require('../middleware/auth')
+const express = require("express");
+const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-const Story = require('../models/Story')
+const Story = require("../models/Story");
 
 // @desc    Login/Landing page
 // @route   Get/
 
-router.get('/', ensureGuest, (req, res) => {
-    res.render('login', {
-        layout: 'login'
-    })
-})
+router.get("/", ensureGuest, (req, res) => {
+  res.render("login", {
+    layout: "login",
+  });
+});
 // @desc    Dashboard
 // @route   GET /dashboard
-router.get('/dashboard', ensureAuth, async (req, res) => {
-    try {
-        const stories = await Story.find({
-            user: req.user.id
-        }).lean()
-        res.render('dashboard', {
-            name: req.user.firstName,
-        })
-    } catch (err) {
-        console.log(err)
-        res.render('error/500')
-    }
-})
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.user.id,
+    }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.log(err);
+    res.render("error/500");
+  }
+});
 
-
-module.exports = router
+module.exports = router;
